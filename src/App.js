11ed22b039a48f0ab7309db13d92cdc0f00156b7
Christian Morris <116536697+DevChristianMorris@users.noreply.github.com>
2023-01-10 16:15:33 +1300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { getProducts } from "./services/getProducts";
+import FilterBy from "./components/FilterBy";
+import SortBy from "./components/SortBy";
+import Products from "./components/Products";
+
+const setFilterBy = (filterBy) => {
+alert(`Filter by: ${filterBy}`);
+};
+
+const setSortBy = (sortBy) => {
+  alert(`Sortby: ${sortBy}`);
+};
+
+const App = () => {
+  const products = getProducts();
+
+  return (
+    <div className="container">
+      <h1>Sunglass Shop</h1>
+      <div className="toolbar">
+        <FilterBy setFilterBy={setFilterBy}/>
+        <SortBy setSortBy={setSortBy}/>
+      </div>
+      <Products products={products} />
+    </div>
+  );
+};
+
+export default App;
